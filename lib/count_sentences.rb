768e3997cad2f009667130b54ebf2db07ba0c8a4
SuperPox <calebkohl@gmail.com>
2021-01-21 21:42:25 -0700

    commit tree 1e1496144b48ca5d304a6edf4466116103592373

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
-  def count_sentences
-
+  def count_sentences #expect("one. two. three?".count_sentences).to eq(3)
+    #return self.split('.' || '?' || '!').count
+    return self.split(/[?!.]\s+/).length
+    #binding.pry
   end
 end
+
+#complex_string = "This, well, is a sentence. // This is too! // ! // And so is this, I think? Woo..."
